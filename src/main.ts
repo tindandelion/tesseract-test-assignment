421e41186812988372809db10a6f47c5d60f9717
Sergey Moshnikov <s.moshnikov@gmail.com>
2022-07-06 13:44:16 +0300

    Implementing SQL variants of UserRepository and DepositLedger

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,56 @@ function badRequest(res: express.Response, message: string) {
   res.status(400).send(message);
 }
 
+function getUserList(userRepo: UserRepository, depositLedger: DepositLedger) {
+  return asyncHandler(async (_req, res) => {
+    const allUsers = await userRepo.getAll();
+    const response = await Promise.all(
+      allUsers.map(async u => ({
+        ...u,
+        balance: await depositLedger.getBalance(u.id),
+      }))
+    );
+    res.json(response);
+  });
+}
+
+function addUser(userRepo: UserRepository) {
+  return asyncHandler(async (req, res) => {
+    const {email} = req.body;
+
+    if (await userRepo.userExistsByEmail(email)) {
+      badRequest(res, `User with email [${email}] already exists`);
+    } else {
+      const newUser = await userRepo.addUser(email);
+      res.status(201).json(newUser);
+    }
+  });
+}
+
+function depositAmount(userRepo: UserRepository, depositLedger: DepositLedger) {
+  async function balanceWillGoBelowZero(userId: number, depositAmount: number) {
+    const balance = await depositLedger.getBalance(userId);
+    return balance + depositAmount < 0;
+  }
+
+  return asyncHandler(async (req, res) => {
+    const {userId, amount} = req.body;
+
+    const userExists = await userRepo.userExistsById(userId);
+    if (!userExists) {
+      badRequest(res, `User by id [${userId}] does not exist`);
+    } else if (await balanceWillGoBelowZero(userId, amount)) {
+      badRequest(
+        res,
+        `Too big amount to withdraw: [${amount}] from user by id: [${userId}]`
+      );
+    } else {
+      const newDeposit = await depositLedger.deposit(userId, amount);
+      res.status(201).json(newDeposit);
+    }
+  });
+}
+
 export function createApp(params: ApplicationDependencies) {
   const {userRepository, depositLedger} = params;
 
@@ -21,54 +71,9 @@ export function createApp(params: ApplicationDependencies) {
     res.send('pong');
   });
 
-  app.get(
-    '/api/users',
-    asyncHandler(async (_req, res) => {
-      const allUsers = await userRepository.getAll();
-      const response = await Promise.all(
-        allUsers.map(async u => ({
-          ...u,
-          balance: await depositLedger.getBalance(u.id),
-        }))
-      );
-      res.json(response);
-    })
-  );
-
-  app.post(
-    '/api/users',
-    asyncHandler(async (req, res) => {
-      const {email} = req.body;
-      if (await userRepository.userExistsByEmail(email)) {
-        badRequest(res, `User with email [${email}] already exists`);
-      } else {
-        const newUser = await userRepository.addUser(email);
-        res.status(201).json(newUser);
-      }
-    })
-  );
-
-  app.post(
-    '/api/deposits',
-    asyncHandler(async (req, res) => {
-      const {userId, amount} = req.body;
-      const userExists = await userRepository.userExistsById(userId);
-      if (!userExists) {
-        badRequest(res, `User by id [${userId}] does not exist`);
-      } else {
-        const balance = await depositLedger.getBalance(userId);
-        if (balance + amount < 0) {
-          badRequest(
-            res,
-            `Too big of amount to withdraw: [${amount}] from user by id: [${userId}]`
-          );
-        } else {
-          const newDeposit = await depositLedger.deposit(userId, amount);
-          res.status(201).json(newDeposit);
-        }
-      }
-    })
-  );
+  app.get('/api/users', getUserList(userRepository, depositLedger));
+  app.post('/api/users', addUser(userRepository));
+  app.post('/api/deposits', depositAmount(userRepository, depositLedger));
 
   return app;
 }
--- test/sql-implementations.spec.ts
+++ test/sql-implementations.spec.ts
@@ -6,7 +6,7 @@ import {
   Model,
   Sequelize,
 } from 'sequelize';
-import {User} from '../src/core-types';
+import {Deposit, DepositLedger, User, UserRepository} from '../src/core-types';
 
 class UserModel extends Model<
   InferAttributes<UserModel>,
@@ -14,6 +14,15 @@ class UserModel extends Model<
 > {
   declare id: CreationOptional<number>;
   declare email: string;
+}
+
+class DepositModel extends Model<
+  InferAttributes<DepositModel>,
+  InferCreationAttributes<DepositModel>
+> {
+  declare id: CreationOptional<number>;
+  declare userId: number;
+  declare amount: number;
 }
 
 function initModels(sequelize: Sequelize) {
@@ -24,13 +33,26 @@ function initModels(sequelize: Sequelize) {
     },
     {tableName: 'Users', sequelize}
   );
+  DepositModel.init(
+    {
+      id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+      userId: {type: DataTypes.INTEGER, allowNull: false},
+      amount: {type: DataTypes.DECIMAL, allowNull: false},
+    },
+    {tableName: 'Deposits', sequelize}
+  );
+  DepositModel.belongsTo(UserModel, {foreignKey: 'userId'});
 }
 
 function toUser(model: UserModel): User {
   return {id: model.id, email: model.email};
 }
 
-class SqlUserRepository {
+function toDeposit(model: DepositModel): Deposit {
+  return {id: model.id, userId: model.userId, amount: model.amount};
+}
+
+class SqlUserRepository implements UserRepository {
   async addUser(email: string) {
     const model = await UserModel.create({email});
     return toUser(model);
@@ -47,6 +69,18 @@ class SqlUserRepository {
   async getAll(): Promise<User[]> {
     const models = await UserModel.findAll();
     return models.map(toUser);
+  }
+}
+
+class SqlDepositLedger implements DepositLedger {
+  async getBalance(userId: number): Promise<number> {
+    const value = await DepositModel.sum('amount', {where: {userId}});
+    return value ?? 0;
+  }
+
+  async deposit(userId: number, amount: number): Promise<Deposit> {
+    const model = await DepositModel.create({userId, amount});
+    return toDeposit(model);
   }
 }
 
@@ -81,5 +115,44 @@ describe(SqlUserRepository, () => {
 
     expect(await repo.userExistsByEmail(email)).toBeTruthy();
     expect(await repo.userExistsByEmail('absent-' + email)).toBeFalsy();
+  });
+});
+
+describe('SqlDepositLedger', () => {
+  let userRepo: SqlUserRepository;
+  let ledger: SqlDepositLedger;
+  let userId: number;
+
+  beforeEach(async () => {
+    const sequelize = new Sequelize('sqlite::memory:', {logging: false});
+    initModels(sequelize);
+    await sequelize.sync();
+
+    ledger = new SqlDepositLedger();
+    userRepo = new SqlUserRepository();
+
+    userId = (await userRepo.addUser('test@example.com')).id;
+  });
+
+  it('returns zero balance if no deposits are registered', async () => {
+    expect(await ledger.getBalance(userId)).toEqual(0);
+  });
+
+  it('registers the deposit', async () => {
+    const deposit = await ledger.deposit(userId, 100);
+    expect(deposit).toEqual({id: 1, userId, amount: 100});
+    expect(await ledger.getBalance(userId)).toEqual(100);
+  });
+
+  it('sums the balance by user', async () => {
+    const anotherUserId = (await userRepo.addUser('another-user@example.com'))
+      .id;
+
+    await ledger.deposit(userId, 100);
+    await ledger.deposit(userId, -20);
+    await ledger.deposit(anotherUserId, 10);
+
+    expect(await ledger.getBalance(userId)).toEqual(80);
+    expect(await ledger.getBalance(anotherUserId)).toEqual(10);
   });
 });
